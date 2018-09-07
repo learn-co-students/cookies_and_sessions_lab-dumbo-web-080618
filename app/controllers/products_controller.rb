477cf6c39acd0669a5261f4360c45f627e15a2bc
skarimo <karimovj@gmail.com>
2018-09-07 17:22:59 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+
+
+  def index
+    @cart = cart
+  end
+
+  def add
+
+    @product = params[:product]
+    cart << @product
+
+    redirect_to "/"
+  end
+end
