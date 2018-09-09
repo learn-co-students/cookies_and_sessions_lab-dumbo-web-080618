96b67d2df72273ebe6ce0fdc48879ccb2ef626b9
github username <github email address>
2018-09-09 14:22:37 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+
+  def index
+    @cart = cart
+
+  end
+
+  def add
+
+    @cart = cart
+    @cart << params["product"]
+    redirect_to products_path
+  end
+
+end
